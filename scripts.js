e7767e0f6a6aaf7edd8c8da1473539c6d171421d
KonradSand <56048957+KonradSand@users.noreply.github.com>
2021-11-19 03:35:42 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@
 
 //Funksjon for "Jeg vil vite mer"-knappen på forsiden
 function visHistorie() {
+      var http = require('http');
+    http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Hello World\n');
+    }).listen(1337, "127.0.0.1");
+    console.log('Server running at http://127.0.0.1:1337/');
+
   var x = document.getElementById("historie");
   var y = document.getElementById("historieKnapp");
   if (x.style.display === "none") {
